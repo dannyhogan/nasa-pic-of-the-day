63bd63145851541e6aaa0732225866b30572e55a
Dan Hogan <hogan.daniel@hotmail.com>
2020-02-12 10:14:48 -0800

    Pass state to PictureDisplay component

--- src/containers/PicOfTheDay/PicOfTheDay.js
+++ src/containers/PicOfTheDay/PicOfTheDay.js
@@ -7,7 +7,7 @@ import { getPicture } from '../../services/nasaApi';
 const PicOfTheDay = () => {
 
   const [picture, setPicture] = useState({});
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [date, setDate] = useState(new Date());
 
 
@@ -23,7 +23,7 @@ const PicOfTheDay = () => {
   return (
     <section className="PicOfTheDay">
       <DateSearch date={date} setDate={setDate} />
-      <PictureDisplay picture={picture} loading={loading} />
+      <PictureDisplay picture={picture} loading={loading} date={date} />
     </section>
   )
 };
