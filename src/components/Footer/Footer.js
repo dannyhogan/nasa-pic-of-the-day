c5df5188e38fb6269cca70af6cf245caafdbc78d
Dan Hogan <hogan.daniel@hotmail.com>
2020-02-12 10:13:54 -0800

    Add links and content to Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,6 +5,13 @@ const Footer = () => {
   return (
     <footer className="Footer">
       <h3>Developed by Danny Hogan</h3>
+      <div className="socials">
+        <a href="https://github.com/dannyhogan">Github</a>
+        <span>•</span>
+        <a href="https://www.linkedin.com/in/danny-hogan/">LinkedIn</a>
+        <span>•</span>
+        <a href="http://dannyhogan.dev/">Website</a>
+      </div>
     </footer>
   )
 };
