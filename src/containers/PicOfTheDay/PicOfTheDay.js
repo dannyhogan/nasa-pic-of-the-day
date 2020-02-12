e01c127de6417f6c8289c43525b79255f0e89864
Dan Hogan <hogan.daniel@hotmail.com>
2020-02-12 00:45:19 -0800

    Hook up state to picture display, use service to fetch picture

--- src/containers/PicOfTheDay/PicOfTheDay.js
+++ src/containers/PicOfTheDay/PicOfTheDay.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './PicOfTheDay.css';
 import DateSearch from '../../components/DateSearch/DateSearch';
 import PictureDisplay from '../../components/PictureDisplay/PictureDisplay'
+import { getPicture } from '../../services/nasaApi';
 
 const PicOfTheDay = () => {
+
+  const [picture, setPicture] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    getPicture()
+      .then(picture => {
+        setPicture(picture);
+        setLoading(false);
+      })
+  }, [])
+
   return (
     <section className="PicOfTheDay">
       <DateSearch />
-      <PictureDisplay />
+      <PictureDisplay picture={picture} loading={loading} />
     </section>
   )
 };
