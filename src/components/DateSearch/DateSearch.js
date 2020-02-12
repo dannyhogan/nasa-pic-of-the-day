608c2eb6c36b95e53386f1b7bbeff3f55a694472
Dan Hogan <hogan.daniel@hotmail.com>
2020-02-12 01:13:02 -0800

    Create DateSearch component, connect to state

--- src/components/DateSearch/DateSearch.js
+++ src/components/DateSearch/DateSearch.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './DateSearch.css';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
-const DateSearch = () => {
+
+const DateSearch = ({ date, setDate }) => {
+
   return (
     <div className="DateSearch">
-      <h1>Date Search</h1>
+      <h2>
+        Pick a date!
+      </h2>
+      <h4>Pictures every day by NASA since July 16th, 1995.</h4>
+      <DatePicker
+        selected={date}
+        onChange={setDate}
+      />
     </div>
   )
 };
