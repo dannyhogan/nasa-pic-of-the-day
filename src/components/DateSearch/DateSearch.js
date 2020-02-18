5d189d6270e01dc0bbe5ec0016c1bfe99e2579ef
Dan Hogan <hogan.daniel@hotmail.com>
2020-02-18 10:43:25 -0800

    Normalize date to string in state

--- src/components/DateSearch/DateSearch.js
+++ src/components/DateSearch/DateSearch.js
@@ -12,20 +12,24 @@ const DateSearch = ({ date, setDate }) => {
     const randomMonth = Math.ceil(Math.random() * 12)
     const randomDay = Math.ceil(Math.random() * 31)
     const randomYear = currentYear - Math.floor(Math.random() * (currentYear - 1995))
-    const randomDate = new Date(`${randomMonth}-${randomDay}-${randomYear}`);
-    console.log('random date generated', date);
-    setDate(randomDate);
+    const randomDate = `${randomMonth}-${randomDay}-${randomYear}`;
+    setDate(randomDate)
   }
+
+  const handleDateChange = ({ target }) => {
+    console.log(target.value);
+    setDate(target.value)
+  };
 
   return (
     <section className="DateSearch">
       <div className="searchContainer">
         <h2 className="searchHeader">Pick a date between now and July 16th, 1995.</h2>
         <div className="search">
-          <DatePicker
+          <input
+            type="date"
             className="datePicker"
-            selected={date}
-            onChange={setDate}
+            onChange={handleDateChange}
           />
           <h3 className="searchH3"> - OR - </h3>
           <Button type="button" id="searchButton" onClick={getRandomPicture}>Random picture!</Button>
