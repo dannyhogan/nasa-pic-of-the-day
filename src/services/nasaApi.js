2a903891f56e4933b2645ef4eec5916563baccaf
Dan Hogan <hogan.daniel@hotmail.com>
2020-02-18 10:43:37 -0800

    Change fetch to handle string instead of date

--- src/services/nasaApi.js
+++ src/services/nasaApi.js
@@ -1,14 +1,9 @@
 export const getPicture = (date) => {
 
-  let dateString;
-  if(date) {
-    dateString = date.toISOString().split('T')[0];
-  }
-
-  return fetch(`https://apodapi.herokuapp.com/api/?date=${dateString}`)
+  return fetch(`https://apodapi.herokuapp.com/api/?date=${date}`)
     .then(res => ([res.ok, res.json()]))
     .then(([ok, json]) => {
-      if(!ok) throw new Error(`Unable to fetch picure for ${dateString}`);
+      if(!ok) throw new Error(`Unable to fetch picure for ${date}`);
       return json
     });
 };
